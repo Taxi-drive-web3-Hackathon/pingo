27ebfcd639c01fb96068cb64491c1a8d2a976ce1
Luan Pontolio <luan.pontolio@gmail.com>
2023-12-11 01:35:55 -0300

    add scripts

---
+++ scripts/src/source.js
@@ -0,0 +1,40 @@
+const paymentId = args[0];
+
+if (!secrets.token) {
+  throw new Error('Missing token');
+}
+
+// build HTTP request object
+
+const request = Functions.makeHttpRequest({
+  url: `https://pingo-pay.vercel.app/payments?id=${paymentId}`,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + secrets.token,
+  },
+});
+
+// Make the HTTP request
+const response = await request;
+
+if (response.error) {
+  throw new Error('PinGo failed to fetch payment details' + response.error);
+}
+
+// fetch the price
+const chainIds = {
+  80001: 1,
+  43113: 2,
+};
+
+const chainId = chainIds[Number(response.data.chainId)];
+const values = Object.values({
+  id: Number(response.data.id),
+  addressReceiver: Number(response.data.addressReceiver),
+  chainId,
+  amount: Number(response.data.addressReceiver).toString(),
+});
+
+console.log(values.toString());
+
+return Functions.encodeString(values.toString());
